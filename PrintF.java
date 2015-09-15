2e69aaba77ac3764b7cdb7b6a4d0e8241dc9b8e8
kkane106 <kkane106@gmail.com>
2015-09-15 09:07:02 -0600

    Add user input to variable stores, check for age to convert plurality of years

--- PrintF.java
+++ PrintF.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+
 public class PrintF {
 	public static void main(String[] args) {
-		int age = 28;
-		String name = "Kris";
+		int age;
+		String name, plural;
+		Scanner keyboard = new Scanner(System.in);
 
-		System.out.printf("%s is %d years old\n",name, age);
+		System.out.print("Enter your name: ");
+		name = keyboard.next();
+
+		System.out.print("Enter you age: ");
+		age = keyboard.nextInt();
+
+		if (age == 1) {
+			plural = "year";
+		} else {
+			plural = "years";
+		}
+
+		keyboard.close();
+		
+		System.out.printf("%s is %d %s old\n",name, age, plural);
+
 	}
 }
